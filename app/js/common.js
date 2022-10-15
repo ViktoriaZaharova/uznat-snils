2359c6fbe17d6ad149f3356a56e08f9ad9e2101b
Viktoria <zaharova181114@gmail.com>
2022-10-15 20:45:37 +0300

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,21 @@
-$('.js-example-basic-single').select2({
-    placeholder: "Поиск региона",
-    width: "100%"
-});
-
 $('.btn-burger').on('click', function () {
     $(this).toggleClass('click');
    $('header .menu').fadeToggle();
+});
+
+$('.examination-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    infinite: false,
+    // arrows: false,
+    prevArrow: $('.btn-prev-slide'),
+    nextArrow: $('.btn-next-slide'),
+    adaptiveHeight: true
+});
+
+
+
+$('.js-example-basic-single').select2({
+    placeholder: "Поиск региона",
+    width: "100%"
 });
